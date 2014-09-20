51a0948e75ce055d3f5a7050ddb518c851fd03bb
Josiah Kerley <josiahkerley@gmail.com>
2014-09-20 17:25:57 -0600

    more

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 
 
 ## Setup
-yum install -y docker
+rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum update -y
+yum install -y docker-io
+service docker start
+chkconfig docker on
 bash nginx-ssl-setup.sh
 bash build-containers.sh
 python DockProx.py &
