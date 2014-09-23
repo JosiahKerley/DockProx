e6e00793c8d1316fd88cd686140897f6658da6ee
Josiah Kerley <josiah.a.kerley@centurylink.com>
2014-09-22 20:04:06 -0600

    Mon Sep 22 20:04:06 MDT 2014

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 ## Setup
 rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum update -y
-yum install -y docker-io
+yum install -y docker-io python-pip
 service docker start
 chkconfig docker on
 pip install docker-py
