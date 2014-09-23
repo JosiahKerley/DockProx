f8ed209c07f3fd566da82b1c921a9bf23c142f67
Josiah Kerley <josiah.a.kerley@centurylink.com>
2014-09-23 12:34:28 -0600

    Tue Sep 23 12:34:28 MDT 2014

--- containers/splash/strap-splash.sh
+++ containers/splash/strap-splash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install -y apache2 || yum install -y httpd
-#rm -rf /var/www/html/*
-#cp -r ./html/* /var/www/html/
+rm -rf /var/www/html/*
+cp -r ./html/* /var/www/html/
 chkconfig httpd on
 service httpd restart
