019b503ad68943c7ddb6e50e42453440ec043270
Josiah Kerley <josiah.a.kerley@centurylink.com>
2014-09-23 13:32:38 -0600

    Tue Sep 23 13:32:38 MDT 2014

--- build-containers-ngid.sh
+++ build-containers-ngid.sh
@@ -5,6 +5,13 @@
 docker build -t="apsd-etherpad.ngid.centurylink.net" github.com/arcus-io/docker-etherpad
 docker run -d "apsd-etherpad.ngid.centurylink.net"
 
+docker build -t="apsd-collab00.ngid.centurylink.net" ./containers/docker-lampstack
+docker run -d "apsd-collab00.ngid.centurylink.net"
+
+docker build -t="apsd-meeting.ngid.centurylink.net" ./containers/bigbluebutton
+docker run -d "apsd-meeting.ngid.centurylink.net"
+
+
 
 ## Display
 clear
