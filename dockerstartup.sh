9f9cedb4495c50cd17c3b6f05e0f8434a664cd92
Josiah Kerley <josiah.a.kerley@centurylink.com>
2014-09-30 09:52:35 -0600

    Tue Sep 30 09:52:35 MDT 2014

--- dockerstartup.sh
+++ dockerstartup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+while read entry
+do
+	name=`echo $entry | cut -d , -f1`
+	data=`echo $entry | cut -d , -f2`
+	docker run -d -v $data "$name"
+done</etc/dockerstartup-persistant
+
+
 while read name
 do
 	docker run -d "$name"
