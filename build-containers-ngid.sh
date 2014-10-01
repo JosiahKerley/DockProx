05d449f622182d9155d554bd68d9dd840fcb3699
Josiah Kerley <josiah.a.kerley@centurylink.com>
2014-10-01 11:56:28 -0600

    Wed Oct  1 11:56:28 MDT 2014

--- build-containers-ngid.sh
+++ build-containers-ngid.sh
@@ -16,7 +16,7 @@ if ! grep -qe "$name" "$startup" ; then echo "$name,$share:/opt/etherpad/var" >>
 name=apsd-collab00.ngid.centurylink.net
 startup=/etc/dockerstartup-persistant
 share=/media/docker/splash/html
-docker build -t="$name" --no-cache ./containers/docker-lampstack
+docker build -t="$name" --no-cache ./containers/splash
 mkdir -p "$share"
 if ! grep -qe "$name" "$startup" ; then echo "$name,$share:/var/www/html" >> "$startup"
 
